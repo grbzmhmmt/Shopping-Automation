283cc1894d5ba26a931ae23db63d34366538a890
grbzmhmmt <grbzmhmmt@gmail.com>
2019-02-22 14:19:21 +0300

    (click) and *ngIf
    Event Binding(click) and '*ngIf' using

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -8,21 +8,27 @@ import { HttpClient } from '@angular/common/http';
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
-  providers:[ProductService]
+  providers: [ProductService]
 })
 export class ProductComponent implements OnInit {
-  
-  products:Product[];
-  constructor(private productService:ProductService,private httpClient: HttpClient,@Inject('apiUrl') private apiUrl) { }
+
+  products: Product[];
+  addedProduct:string;
+  constructor(private productService: ProductService, private httpClient: HttpClient, @Inject('apiUrl') private apiUrl) { }
 
   ngOnInit() {
     this.getProducts();
   }
 
   getProducts() {
-    this.httpClient.get<Product[]>(this.apiUrl+'/products').subscribe(response => {
-      this.products=response;
+    this.httpClient.get<Product[]>(this.apiUrl + '/products').subscribe(response => {
+      this.products = response;
     });
+  }
 
-}
+  AddToCart(product:Product){
+    this.addedProduct=product.productName;
+    
+  }
+  
 }
