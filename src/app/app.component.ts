752f31b944f0578a77d39020740f5bd6df18fb98
grbzmhmmt <grbzmhmmt@gmail.com>
2019-03-11 19:11:40 +0300

    Adding introjs library
    Adding introjs library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var require: any
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,5 +10,39 @@ import { Component } from '@angular/core';
 export class AppComponent {
   
   title = 'Northwind Project';
+
+
+  introStart(){
+    const IntroJs=require('../../node_modules/intro.js/intro.js');
+    let intro=IntroJs();
+
+    intro.setOptions({
+      steps:[
+        {
+          intro:'Welcome to Northwind Project',
+          position:'bottom'
+        },{
+          element:'#intro1',
+          intro:'Navbar and Your Cart Summary You can see hear your shopping cart',
+          position:'left'
+        },{
+          element:'#intro2',
+          intro:'You can see hear my different angular properties',
+          position:'right'
+        },{
+          element:'#intro3',
+          intro:'Hear is product categories',
+          position:'right'
+        },{
+          element:'#intro4',
+          intro:'Products Details You can see Products and their properties',
+          position:'top'
+        }
+      ]
+    });
+    intro.start();
+  }
+
+
 
 }
